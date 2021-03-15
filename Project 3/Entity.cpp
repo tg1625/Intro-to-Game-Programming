d4423268085fb53909bffbff8d443d15d5820f50
tg1625 <35944491+tg1625@users.noreply.github.com>
2021-03-15 15:21:05 -0400

    finish project 3

---
+++ Project 3/Entity.cpp
@@ -0,0 +1,182 @@
+#include "Entity.h"
+
+Entity::Entity()
+{
+    position = glm::vec3(0);
+    movement = glm::vec3(0);
+    acceleration = glm::vec3(0);
+    velocity = glm::vec3(0);
+    speed = 0;
+
+    modelMatrix = glm::mat4(1.0f);
+}
+
+void Entity::Update(float deltaTime, Entity* platforms, int platformCount, Entity* flag)
+{
+    if (entityType == EntityType::PLAYER) {
+        collidedTop = false;
+        collidedBottom = false;
+        collidedLeft = false;
+        collidedRight = false;
+
+        if (animIndices != NULL) {
+            if (!isLost && !isWon) {
+                animTime += deltaTime;
+                if (animTime >= 0.25f)
+                {
+                    animTime = 0.0f;
+                    animIndex++;
+                    if (animIndex >= animFrames - 1)
+                    {
+                        animIndex = 0;
+                    }
+                }
+            }
+            else if(isLost) {
+                animIndex = 3;
+            }
+            else {
+                animIndex = 1;
+            }
+        }
+        
+
+        if (!isActive) return;
+
+        if (!isLost && !isWon) {
+            velocity += acceleration * deltaTime;
+
+            position.y += velocity.y * deltaTime; // Move on Y
+            CheckCollisionsY(flag, 1);// Fix if needed
+            CheckCollisionsY(platforms, platformCount);// Fix if needed
+
+            position.x += velocity.x * deltaTime; // Move on X
+            CheckCollisionsX(platforms, platformCount);// Fix if needed
+        }
+    }
+
+    modelMatrix = glm::mat4(1.0f);
+    modelMatrix = glm::translate(modelMatrix, position);
+}
+
+void Entity::DrawSpriteFromTextureAtlas(ShaderProgram* program, GLuint textureID, int index)
+{
+    float u = (float)(index % animCols) / (float)animCols;
+    float v = (float)(index / animCols) / (float)animRows;
+
+    float width = 1.0f / (float)animCols;
+    float height = 1.0f / (float)animRows;
+
+    float texCoords[] = { u, v + height, u + width, v + height, u + width, v,
+        u, v + height, u + width, v, u, v };
+
+    float vertices[] = { -0.5, -0.5, 0.5, -0.5, 0.5, 0.5, -0.5, -0.5, 0.5, 0.5, -0.5, 0.5 };
+
+    glBindTexture(GL_TEXTURE_2D, textureID);
+
+    glVertexAttribPointer(program->positionAttribute, 2, GL_FLOAT, false, 0, vertices);
+    glEnableVertexAttribArray(program->positionAttribute);
+
+    glVertexAttribPointer(program->texCoordAttribute, 2, GL_FLOAT, false, 0, texCoords);
+    glEnableVertexAttribArray(program->texCoordAttribute);
+
+    glDrawArrays(GL_TRIANGLES, 0, 6);
+
+    glDisableVertexAttribArray(program->positionAttribute);
+    glDisableVertexAttribArray(program->texCoordAttribute);
+}
+
+void Entity::CheckCollisionsY(Entity* objects, int objectCount)
+{
+    for (int i = 0; i < objectCount; i++)
+    {
+        Entity* object = &objects[i];
+        if (CheckCollision(object))
+        {
+            if (!isWon) {
+                if (object->entityType == EntityType::WALL) {
+                    isLost = true;
+                }
+                else if (object->entityType == EntityType::GRASS) {
+                    isWon = true;
+                }
+            }
+            float ydist = fabs(position.y - object->position.y);
+            float penetrationY = fabs(ydist - (height / 2.0f) - (object->height / 2.0f));
+            if (velocity.y > 0) {
+                position.y -= penetrationY;
+                velocity.y = 0;
+                collidedTop = true;
+            }
+            else if (velocity.y < 0) {
+                position.y += penetrationY;
+                velocity.y = 0;
+                collidedBottom = true;
+            }
+        }
+    }
+}
+
+void Entity::CheckCollisionsX(Entity* objects, int objectCount)
+{
+    for (int i = 0; i < objectCount; i++)
+    {
+        Entity* object = &objects[i];
+        if (CheckCollision(object))
+        {
+            if (!isWon) {
+                if (object->entityType == EntityType::WALL) isLost = true;
+            }
+            float xdist = fabs(position.x - object->position.x);
+            float penetrationX = fabs(xdist - (width / 2.0f) - (object->width / 2.0f));
+            if (velocity.x > 0) {
+                position.x -= penetrationX;
+                velocity.x = 0;
+                collidedRight = true;
+            }
+            else if (velocity.x < 0) {
+                position.x += penetrationX;
+                velocity.x = 0;
+                collidedLeft = true;
+            }
+        }
+    }
+}
+
+
+void Entity::Render(ShaderProgram* program) {
+    program->SetModelMatrix(modelMatrix);
+
+    if (animIndices != NULL && entityType == EntityType::PLAYER) {
+        DrawSpriteFromTextureAtlas(program, textureID, animIndices[animIndex]);
+        return;
+    }
+
+    float vertices[] = { -0.5, -0.5, 0.5, -0.5, 0.5, 0.5, -0.5, -0.5, 0.5, 0.5, -0.5, 0.5 };
+    float texCoords[] = { 0.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0 };
+
+    glBindTexture(GL_TEXTURE_2D, textureID);
+
+    glVertexAttribPointer(program->positionAttribute, 2, GL_FLOAT, false, 0, vertices);
+    glEnableVertexAttribArray(program->positionAttribute);
+
+    glVertexAttribPointer(program->texCoordAttribute, 2, GL_FLOAT, false, 0, texCoords);
+    glEnableVertexAttribArray(program->texCoordAttribute);
+
+    glDrawArrays(GL_TRIANGLES, 0, 6);
+
+    glDisableVertexAttribArray(program->positionAttribute);
+    glDisableVertexAttribArray(program->texCoordAttribute);
+}
+
+
+bool Entity::CheckCollision(Entity* other) {
+    if (!isActive || !other->isActive) return false;
+
+    float xdist = fabs(position.x - other->position.x) - ((width + other->width) / 2.0f);
+    float ydist = fabs(position.y - other->position.y) - ((height + other->height) / 2.0f);
+
+    if (xdist < 0 && ydist < 0) return true;
+
+    return false;
+}
